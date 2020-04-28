f263803bf10b787d399b09f90bc67385847dcb31
Ashiqur Rahman <arkshykot@gmail.com>
2020-04-28 16:23:25 +0600

    new data type practise in hello.js file

--- hello.js
+++ hello.js
@@ -37,3 +37,53 @@ let myBio = [];
 let addMyBio = myBio[0] +" "+ myBio[1]+ " " + myBio[2] +" "+ myBio[3];
 
 console.log(addMyBio);
+
+
+
+
+
+
+
+
+		/*write down the memory space and call them
+		============================================
+		 karim is bad boy
+		 karim is a "bad" boy
+		*/
+		let complimentA ="karim is bad boy";
+		console.log(complimentA);
+
+		let complimentB = "Karim is a \"bad\" boy";
+		console.log(complimentB);
+
+		/*checkimg the type and explain your answer
+		==========================================
+			43
+			"43"
+		*/
+
+		let numberOne = 43;
+		console.log(numberOne);
+		//numberOne value is a number type data
+
+		let numberTwo = "43";
+		console.log(numberTwo);
+		//numberTwo value is a string type data
+
+		/* write my bio
+		================
+		 I am Ashiqur Rahman.I am 25. I'm web developer. I love "javascropt".
+		 4 data in 4 memory location or reference
+		 Addition 4 data in save another memory reference
+		 call the reference 
+		*/
+
+		let myName = "I am Ashiqur Rahman. ";
+		let age = "I am 25. ";
+		let profession = "I'm web developer. "	;	
+		let interest = "I love \"javascropt\"."; 
+
+		let myBio = myName + age + profession + interest;
+
+		console.log(myBio);
+
