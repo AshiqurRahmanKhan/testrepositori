e806831c14e25423fccdc5d71b309d1eeca2cdd0
Ashiqur Rahman <arkshykot@gmail.com>
2020-04-28 16:07:55 +0600

    Array practise in hello.js file

--- hello.js
+++ hello.js
@@ -3,3 +3,37 @@ console.log("Hello gitHub");
 let name = "Ashiqur"
 
 console.log(name);
+
+
+
+
+
+
+
+/* write my Bio(By grouping closely related data )
+===============================================
+I am Ashiqur Rahman & 25. I'm web developer. I love "javascript".
+
+1. Create a Array
+2. Data store
+3. Addition
+4. Store in into a reference
+5. Calling the reference 
+
+*/
+
+
+// 1. Create Array and 2 data s
+
+let myBio = [];
+
+// 2. Data store
+	
+	myBio = ["I am Ashiqur Rahman", "& 25.", "I\'m web developer.", "I love \"javascript\"."];
+
+
+// 3 & 4. Addition and store in a reference
+
+let addMyBio = myBio[0] +" "+ myBio[1]+ " " + myBio[2] +" "+ myBio[3];
+
+console.log(addMyBio);
